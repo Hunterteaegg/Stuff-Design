a5263941bf8e55c58e6d97a21b35dfb8d84774f2
hunterteaegg <hunterteaegg@126.com>
2020-10-18 13:14:23 +0800

    add all function

--- AirDetect/Perpherial/Src/bluetooth.c
+++ AirDetect/Perpherial/Src/bluetooth.c
@@ -12,19 +12,54 @@ extern uint8_t dht11_data[5];
 extern uint16_t gy30_data;
 
 uint8_t string_humidity[]="humidity:";
+uint8_t string_temperature[]="temperature:";
+uint8_t string_luminance[]="luminance:";
 
-uint8_t space='\n';
+uint8_t line_feed='\n';
+uint8_t space=' ';
 
 void bluetooth_transmit_humi(void)
 {
-	uint8_t temp[2]={
-			0,0,
+	uint8_t temp[3]={
+			0,0,'%',
 	};
 
-	HAL_UART_Transmit(&huart1,string_humidity , sizeof(string_humidity),0xFF);
 	temp[0]=(dht11_data[0] / 10)+48;
 	temp[1]=(dht11_data[0] % 10)+48;
 
+	HAL_UART_Transmit(&huart1,string_humidity , sizeof(string_humidity),0xFF);
+	HAL_UART_Transmit(&huart1, temp, sizeof(temp), 0xFF);
+}
+
+void bluetooth_transmit_temp(void)
+{
+	uint8_t temp[8]={
+			0,0,'.',0,0,0,'`','C',
+	};
+
+	temp[0]=(dht11_data[2] / 10)+48;
+	temp[1]=(dht11_data[2] % 10)+48;
+	temp[3]=(dht11_data[3] / 100)+48;
+	temp[4]=(dht11_data[3] % 100 / 10)+48;
+	temp[5]=(dht11_data[3] % 10)+48;
+
+	HAL_UART_Transmit(&huart1, string_temperature, sizeof(string_temperature), 0xFF);
+	HAL_UART_Transmit(&huart1, temp, sizeof(temp), 0xFF);
+}
+
+void bluetooth_transmit_luminance(void)
+{
+	uint8_t temp[7]={
+			0,0,0,0,0,'L','x',
+	};
+
+	temp[0]=(gy30_data / 10000)+48;
+	temp[1]=(gy30_data % 10000 / 1000)+48;
+	temp[2]=(gy30_data % 1000 / 100)+48;
+	temp[3]=(gy30_data % 100 / 10)+48;
+	temp[4]=(gy30_data % 10)+48;
+
+	HAL_UART_Transmit(&huart1, string_luminance, sizeof(string_luminance), 0xFF);
 	HAL_UART_Transmit(&huart1, temp, sizeof(temp), 0xFF);
 }
 
@@ -32,5 +67,13 @@ void bluetooth_transmit(void)
 {
 	bluetooth_transmit_humi();
 	HAL_UART_Transmit(&huart1, &space, sizeof(space), 0xFF);
+
+	bluetooth_transmit_temp();
+	HAL_UART_Transmit(&huart1, &space, sizeof(space), 0xFF);
+
+	bluetooth_transmit_luminance();
+	HAL_UART_Transmit(&huart1, &space, sizeof(space), 0xFF);
+
+	HAL_UART_Transmit(&huart1, &line_feed, sizeof(line_feed), 0xFF);
 }
 
